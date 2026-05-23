ce28fba3afd6bf4ae0831d70bf8b351cc054a6b8
lottetw <lotteyang@gmail.com>
2026-05-23 12:56:33 +0800

    修改edm

--- js/module.js
+++
@@ -1,9 +0,0 @@
-export function activeCurrentLink(activeId) {
-  $(".jsGoAnchor").removeClass("active");
-  $('[data-link="' + activeId + '"]').addClass("active");
-  var val = $('[data-link="' + activeId + '"]').attr("value");
-  if (typeof val !== typeof undefined && val !== false) {
-    $("#jsMobileStatus").text(val);
-  }
-}
-// export { activeCurrentLink };
